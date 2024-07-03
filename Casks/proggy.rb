1bcda9b92c1c40e2c3afeb218f09969998e670f6
Simon Chiu <simon@furvur.com>
2024-07-03 14:50:54 -0400

    Bump to v1.3.20

--- Casks/proggy.rb
+++ Casks/proggy.rb
@@ -1,19 +1,22 @@
 cask "proggy" do
-  version "1.3.8"
+  version "1.3.20"
 
   if Hardware::CPU.intel?
     arch = "amd64"
-    sha256 "b4094e0ff9a1b9d2fb1a166c5b217180c222e00a742bb34d2ca8183e9669da66"
-  else
+    sha256 "784ffd18b9714a910812960efecfe7f3e241b5daadc242c42222d33dcbd49ab2"
+    url "https://github.com/proggy-io/cli/releases/download/v#{version}/proggy-cli_Darwin_x86_64.zip"
+  elsif Hardware::CPU.arm?
     arch = "arm64"
-    sha256 "aeb0a444cd7c744617ea4ee212b12bd53689afac15bdaddcd54c858dc14d11f7"
+    sha256 "0ede6a384ae27fc91ce1757134d40df40e3bf6b889d5802008bc261b7cec6f5e"
+    url "https://github.com/proggy-io/cli/releases/download/v#{version}/proggy-cli_Darwin_arm64.zip"
+  else
+    raise "Unsupported architecture: #{Hardware::CPU.type}"
   end
 
   platform = "darwin"
   extension = "zip"
   fallback_path = "~/Library/Application Support/proggy/config.yml"
 
-  url "https://github.com/proggy-io/cli/releases/download/v#{version}/proggy-#{platform}-#{arch}.#{extension}"
   name "proggy"
   desc "Proggy CLI Tool"
   homepage "https://proggy.io"
@@ -21,7 +24,7 @@ cask "proggy" do
   binary "proggy"
 
   postflight do
-    set_permissions "#{staged_path}/proggy", "0755"
+    set_permissions "#{staged_path}/proggy", '0755'
   end
 
   zap trash: [
