c6e3c24b5c4661099373293c893f77f5b182c23c
Simon Chiu <simon@furvur.com>
2024-07-09 10:27:02 -0400

    Bump to v1.3.23

--- Casks/proggy.rb
+++ Casks/proggy.rb
@@ -1,18 +1,20 @@
 cask "proggy" do
-  version '1.3.21'
+  version "1.3.23"
 
   if Hardware::CPU.intel?
-    arch = 'amd64'
-    sha256 'f138d2223d1bddce5ed64d899fd082f8fb48f766c15c76a1c8be156677585a49'
+    arch = "amd64"
+    sha256 "f537394beacf5b70fcf4d185ac6e7ee227d883833e50610284f9a05f9e5992a5"
     url "https://github.com/proggy-io/cli/releases/download/v#{version}/proggy-cli_Darwin_x86_64.zip"
   elsif Hardware::CPU.arm?
-    arch = 'arm64'
-    sha256 '4c5e45d526b5066fe516f8d88ed37a36b462c0752665e00a908bd4d2d4513c2c'
+    arch = "arm64"
+    sha256 "dfb1f59fd903d9ae1dfeb5da439d597b332f41b36d6497376beca28641a61167"
     url "https://github.com/proggy-io/cli/releases/download/v#{version}/proggy-cli_Darwin_arm64.zip"
   else
     raise "Unsupported architecture: #{Hardware::CPU.type}"
   end
 
+  platform = "darwin"
+  extension = "zip"
   fallback_path = "~/Library/Application Support/proggy/config.yml"
 
   name "proggy"
